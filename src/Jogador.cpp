ffeecee882ded14651e3127dce17622c552349e5
Joao <joaossmauricio@gmail.com>
2020-02-04 15:02:29 -0300

    Corrige erros de animacao

--- src/Jogador.cpp
+++ src/Jogador.cpp
@@ -8,6 +8,8 @@ using namespace std;
 Jogador::Jogador(sf::Texture* textura, sf::Vector2u tamanhoDaImagem, float tempoTroca, float vel) :
     animacao(textura, tamanhoDaImagem, tempoTroca)
 {
+    vivo = true;
+
     linha = 0;
 
     velMax = vel;
@@ -35,12 +37,12 @@ Jogador::~Jogador()
 void Jogador::atualiza(float deltaTempo, char direcao)
 {
     //COMANDOS TECLADO
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && sentidoMovimentoY == 0 && !sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && direcao != 'e' && corpoJogador.getPosition().x - corpoJogador.getSize().x / 2 > BORDA_ESQ)
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && sentidoMovimentoY == 0 && !sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && direcao != 'e' && corpoJogador.getPosition().x - corpoJogador.getSize().x / 2 > BORDA_ESQ && vivo)
     {
         sentidoMovimentoX = -1;
         velocidadeX = velMax;
     }
-    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && sentidoMovimentoY == 0 && !sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && direcao != 'd' && corpoJogador.getPosition().x + corpoJogador.getSize().x / 2 < BORDA_DIR)
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Right) && sentidoMovimentoY == 0 && !sf::Keyboard::isKeyPressed(sf::Keyboard::Left) && direcao != 'd' && corpoJogador.getPosition().x + corpoJogador.getSize().x / 2 < BORDA_DIR && vivo)
     {
         sentidoMovimentoX = 1;
         velocidadeX = velMax;
@@ -50,12 +52,12 @@ void Jogador::atualiza(float deltaTempo, char direcao)
         sentidoMovimentoX = 0;
         velocidadeX = 0;
     }
-    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && sentidoMovimentoX == 0 && !sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && direcao != 'c' && corpoJogador.getPosition().y - corpoJogador.getSize().y / 2 > BORDA_SUP)
+    if(sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && sentidoMovimentoX == 0 && !sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && direcao != 'c' && corpoJogador.getPosition().y - corpoJogador.getSize().y / 2 > BORDA_SUP && vivo)
     {
         sentidoMovimentoY = -1;
         velocidadeY = velMax;
     }
-    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && sentidoMovimentoX == 0 && !sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && direcao != 'b' && corpoJogador.getPosition().y + corpoJogador.getSize().y / 2 < BORDA_INF)
+    else if(sf::Keyboard::isKeyPressed(sf::Keyboard::Down) && sentidoMovimentoX == 0 && !sf::Keyboard::isKeyPressed(sf::Keyboard::Up) && direcao != 'b' && corpoJogador.getPosition().y + corpoJogador.getSize().y / 2 < BORDA_INF && vivo)
     {
         sentidoMovimentoY = 1;
         velocidadeY = velMax;
@@ -73,49 +75,57 @@ void Jogador::atualiza(float deltaTempo, char direcao)
         emMovimento = false;
 
     //ANIMACAO
-    if(sentidoMovimentoX == 1)
+    if(vivo)
     {
-        linha = 11;
-        animacao.setNumFrames(9);
-        ultimaDirecao = 'd';
-    }
-    else if(sentidoMovimentoX == -1)
-    {
-        linha = 9;
-        animacao.setNumFrames(9);
-        ultimaDirecao = 'e';
-    }
-    else if(sentidoMovimentoY == 1)
-    {
-        linha = 10;
-        animacao.setNumFrames(9);
-        ultimaDirecao = 'b';
-    }
-    else if(sentidoMovimentoY == -1)
-    {
-        linha = 8;
-        animacao.setNumFrames(9);
-        ultimaDirecao = 'c';
+        if(sentidoMovimentoX == 1)
+        {
+            linha = 11;
+            animacao.setNumFrames(9);
+            ultimaDirecao = 'd';
+        }
+        else if(sentidoMovimentoX == -1)
+        {
+            linha = 9;
+            animacao.setNumFrames(9);
+            ultimaDirecao = 'e';
+        }
+        else if(sentidoMovimentoY == 1)
+        {
+            linha = 10;
+            animacao.setNumFrames(9);
+            ultimaDirecao = 'b';
+        }
+        else if(sentidoMovimentoY == -1)
+        {
+            linha = 8;
+            animacao.setNumFrames(9);
+            ultimaDirecao = 'c';
+        }
+        else
+        {
+            switch(ultimaDirecao)
+            {
+                case 'c':
+                    linha = 0;
+                    break;
+                case 'e':
+                    linha = 1;
+                    break;
+                case 'b':
+                    linha = 2;
+                    break;
+                case 'd':
+                    linha = 3;
+                    break;
+            }
+            animacao.setNumFrames(0);
+            animacao.setFrameAtual(0);
+        }
     }
     else
     {
-        switch(ultimaDirecao)
-        {
-            case 'c':
-                linha = 0;
-                break;
-            case 'e':
-                linha = 1;
-                break;
-            case 'b':
-                linha = 2;
-                break;
-            case 'd':
-                linha = 3;
-                break;
-        }
-        animacao.setNumFrames(0);
-        animacao.setFrameAtual(0);
+        linha = 20;
+        animacao.setNumFrames(6);
     }
 
     float dx = velocidadeX * sentidoMovimentoX * deltaTempo;
