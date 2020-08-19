c3c15d96135960eec6dcb760ada743419a2d77a4
stephanJoao <52497854+stephanJoao@users.noreply.github.com>
2020-08-19 15:06:16 -0300

    Muda nome

--- main.cpp
+++ main.cpp
@@ -79,7 +79,7 @@ int comparaString(char resp[], char colocados[])
 int main()
 {
     //VARIAVEIS DA CONFIGURACAO
-    sf::RenderWindow window(sf::VideoMode(800, 512), "Jogo Aula 03", sf::Style::Close);
+    sf::RenderWindow window(sf::VideoMode(800, 512), "Jogo Aula 05", sf::Style::Close);
     sf::View view(sf::Vector2f(0, 0), sf::Vector2f(VIEW_HEIGHT, VIEW_HEIGHT));
     srand(time(0));
 
